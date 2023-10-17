4cefc01ed1fc1597ab2d43cd57bd833657cb0e2e
jayvay <kulele2208@gmail.com>
2023-10-18 00:04:00 +0900

    20231018_0003 javaScript 함수 복습

--- javascript/1017/test10.js
+++ javascript/1017/test10.js
@@ -4,7 +4,7 @@
 
 let su1 = 10;
 let su2 = -20;
-let su3 = 3.15;
+let su3 = 3.1555;
 let su4 = -3.14;
 let su5 = 31.5;
 
@@ -12,23 +12,23 @@ let num = 0;
 
 num = Math.max(su1, su2, su3, su4, su5);
 num = Math.min(su1, su2, su3, su4, su5);
-num = Math.ceil(su3);   //무조건 올림
-num = Math.ceil(su4);
-num = Math.floor(su3);  //무조건 내림
-num = Math.floor(su4);
+num = Math.ceil(su3);   //ceil : 무조건 올림
+num = Math.ceil(su4);   //음수라서 올리면 -3
+num = Math.floor(su3);  //floor : 무조건 내림
+num = Math.floor(su4);  //음수라서 내리면 -4
 
 
-num = Math.trunc(su3);
+num = Math.trunc(su3);  //trunc : 소수점 이하 자름
 num = Math.trunc(su4);
 
 num = Math.pow(2,5);    // 2의 5승
 num = Math.abs(su2);    // 절대값
-num = Math.sqrt(16);    // 뭐랬지
+num = Math.sqrt(16);    // 제곱근
 
-num = su3.toFixed(2);   // 매개변수로 지정된 소수 이하 자리수 까지 출력시켜준다.(반올림 처리 한다.)
-// num = su5.toFixed(0);
+num = su3.toFixed(2);   // 매개변수로 지정된 소수 이하 자리수까지 출력시켜준다.(반올림 처리 한다.)
+num = su5.toFixed(0);   //정수..
 
-// num = Math.round(su3);
+num = Math.round(su3); //???
 
 num = Math.random();    // 0 <= 난수 < 1 (실수형 난수)
 num = Math.random() * 10;
